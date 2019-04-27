1b2c98e039a1d0e8c2e2464be8a7d023ade8b33d
JavierCR16 <javier.contrerasm16@gmail.com>
2019-04-26 23:43:17 -0600

    dsbebeytntyntyntyn

--- redNeural.py
+++ redNeural.py
@@ -17,17 +17,13 @@ class RedNeural:
         # Inicializando los pesos de manera aleatoria
         # Valor adicional del bias
         self.pesos_oe : torch.Tensor = torch.rand(self.M, self.D)
-        #self.pesos_oe = torch.Tensor([[-4.3561,  4.3269],[-0.8986,  0.2353],        [-1.7860,  1.5103],        [-3.7588,  3.8514],        [ 5.1884, -5.4514],        [-0.0425, -0.5171],        [ 3.5132, -3.3224],        [-1.7697,  1.4660],        [ 5.4206, -5.7103],        [ 4.4498, -4.5519],        [-4.8896,  4.7710],        [-4.8967,  4.7743],        [-4.9030,  4.7749],        [-1.1180,  0.4828],        [-5.1811,  5.0248]])
         self.pesos_so : torch.Tensor = torch.rand(self.K, self.M)
-        #self.pesos_so = torch.Tensor([[ 0.8236, -0.7595, -0.5422,  0.6657,  2.9399, -0.3755,  0.2504, -0.2873,4.4854,  1.4221,  1.7347,  2.1508,  2.0846, -0.3335,  2.7299]])
         self.salidaFinal = None
         self.salidaOculta = None
         self.entrada = None
         self.etiqueta = None
         self.bias_oculto : torch.Tensor = torch.rand(self.K,1)
-        #self.bias_oculto = torch.Tensor([[-4.3157]])
         self.bias_entrada : torch.Tensor = torch.rand(self.M,1)
-        #self.bias_entrada = torch.Tensor([[-3.0266],[-3.9380],[-3.5800],[-2.9217],[-3.0197],[-3.8357],        [-2.5004],        [-3.5859],        [-3.1173],        [-2.7470],        [-3.1659],        [-3.1658],        [-3.1640],        [-3.8913],        [-3.2581]])
         self.errorXIteracion = [[],[]]
 
 
@@ -86,7 +82,7 @@ class RedNeural:
             sumaError = 0
             for data in range(len(X)):
                 #self.etiqueta = torch.Tensor([T[data].copy()])
-                self.etiqueta = torch.Tensor([T[data]])
+                self.etiqueta = torch.Tensor(T[data])
                 self.calcularPasadaAdelante(X[data].copy())
                 sumaError += self.evaluarClasificacionesErroneas(self.salidaFinal, self.etiqueta)
                 self.calcularPasadaAtras()
@@ -127,16 +123,26 @@ class RedNeural:
 
 
 if __name__ == "__main__":
-    red = RedNeural([2, 15, 1], 0.8, 100)
-    i = [[1,1], [1, 0], [0, 1], [0,0]]
-    t = [0,1,1,0]
+    red = RedNeural([2, 15, 1], 0.5, 100)
+    i = [[1,1], [1, 0], [0, 1], [0,0]]  #[[2,2,54,543,],[],[],[],[],[]]
+    t = [[0],[1],[1],[0]]
     # El copy se usa para pasarlo por valor y no referencia y que no se modifique el i
     red.entrenarRed(2000, i, t)
+
     red.evaluarMuesta([1,1])
     salida = red.salidaFinal
     print("Salida de la pasada hacia adelante: {}".format(salida))
-    # print("pesos_oe ",red.pesos_oe)
-    # print("pesos_so ",red.pesos_so)
-    # print("bias_entrada", red.bias_entrada)
-    # print("bias_oculto", red.bias_oculto)
+
+    red.evaluarMuesta([1,0])
+    salida = red.salidaFinal
+    print("Salida de la pasada hacia adelante: {}".format(salida))
+
+    red.evaluarMuesta([0, 1])
+    salida = red.salidaFinal
+    print("Salida de la pasada hacia adelante: {}".format(salida))
+
+    red.evaluarMuesta([0, 0])
+    salida = red.salidaFinal
+    print("Salida de la pasada hacia adelante: {}".format(salida))
+
     red.graficarError()
