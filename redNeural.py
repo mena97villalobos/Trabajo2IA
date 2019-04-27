9c5a65de386138c393bb47cf19a35eb229c22c13
frquiros <franqm95@gmail.com>
2019-04-26 22:36:19 -0600

    clasifica bien con menos iteraciones

--- redNeural.py
+++ redNeural.py
@@ -17,15 +17,17 @@ class RedNeural:
         # Inicializando los pesos de manera aleatoria
         # Valor adicional del bias
         self.pesos_oe : torch.Tensor = torch.rand(self.M, self.D)
-        #self.pesos_oe = torch.Tensor([[-14.4622,  25.9781],[ -9.8981, -11.9187]])
+        #self.pesos_oe = torch.Tensor([[-4.3561,  4.3269],[-0.8986,  0.2353],        [-1.7860,  1.5103],        [-3.7588,  3.8514],        [ 5.1884, -5.4514],        [-0.0425, -0.5171],        [ 3.5132, -3.3224],        [-1.7697,  1.4660],        [ 5.4206, -5.7103],        [ 4.4498, -4.5519],        [-4.8896,  4.7710],        [-4.8967,  4.7743],        [-4.9030,  4.7749],        [-1.1180,  0.4828],        [-5.1811,  5.0248]])
         self.pesos_so : torch.Tensor = torch.rand(self.K, self.M)
-        #self.pesos_so = torch.Tensor([[5.3039, 5.6377]])
+        #self.pesos_so = torch.Tensor([[ 0.8236, -0.7595, -0.5422,  0.6657,  2.9399, -0.3755,  0.2504, -0.2873,4.4854,  1.4221,  1.7347,  2.1508,  2.0846, -0.3335,  2.7299]])
         self.salidaFinal = None
         self.salidaOculta = None
         self.entrada = None
         self.etiqueta = None
         self.bias_oculto : torch.Tensor = torch.rand(self.K,1)
+        #self.bias_oculto = torch.Tensor([[-4.3157]])
         self.bias_entrada : torch.Tensor = torch.rand(self.M,1)
+        #self.bias_entrada = torch.Tensor([[-3.0266],[-3.9380],[-3.5800],[-2.9217],[-3.0197],[-3.8357],        [-2.5004],        [-3.5859],        [-3.1173],        [-2.7470],        [-3.1659],        [-3.1658],        [-3.1640],        [-3.8913],        [-3.2581]])
         self.errorXIteracion = [[],[]]
 
 
@@ -111,7 +113,7 @@ class RedNeural:
         self.bias_oculto -= self.learningRate * self.delta_salida
 
         # Actualizacion de pesos de Capa de Entrada a Capa de Oculta
-        aux2 = (self.learningRate *( self.delta_oculta * self.entrada).t())
+        aux2 = (self.learningRate *( self.delta_oculta * self.entrada))
         self.pesos_oe -= aux2
 
         self.bias_entrada -= self.learningRate * self.delta_oculta
@@ -125,16 +127,16 @@ class RedNeural:
 
 
 if __name__ == "__main__":
-    red = RedNeural([2, 2, 1], 0.7, 100)
+    red = RedNeural([2, 15, 1], 0.8, 100)
     i = [[1,1], [1, 0], [0, 1], [0,0]]
     t = [0,1,1,0]
     # El copy se usa para pasarlo por valor y no referencia y que no se modifique el i
-    red.entrenarRed(10000, i, t)
+    red.entrenarRed(2000, i, t)
     red.evaluarMuesta([1,1])
     salida = red.salidaFinal
     print("Salida de la pasada hacia adelante: {}".format(salida))
-    print("pesos_oe ",red.pesos_oe)
-    print("pesos_so ",red.pesos_so)
-    print("bias_entrada", red.bias_entrada)
-    print("bias_oculto", red.bias_oculto)
+    # print("pesos_oe ",red.pesos_oe)
+    # print("pesos_so ",red.pesos_so)
+    # print("bias_entrada", red.bias_entrada)
+    # print("bias_oculto", red.bias_oculto)
     red.graficarError()
